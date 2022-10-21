8c825daca6382b129226d10fb1a6582542014cb9
chaosarium <leonluleonlu@gmail.com>
2022-10-21 00:18:46 -0400

    use published frontmatter rather than tag

--- content-preprocess.py
+++ content-preprocess.py
@@ -1,3 +1,5 @@
+# inspired by and based on https://github.com/sspaeti/second-brain-public
+
 import os
 from datetime import datetime
 import shutil
@@ -33,37 +35,37 @@ h4 = "(?m)^#{4}(?!#)(.*)"
 h5 = "(?m)^#{5}(?!#)(.*)"
 h6 = "(?m)^#{6}(?!#)(.*)"
 
+def find_publish_hashtag(second_brain_path: str, copy_to_path: str):
+  """
+  - find `published: true` frontmatter in private folder and move it to public folder.
+  - copies h1 title (`# ..`) into frontmatter as YAML title
+  """
+  for root, dirs, files in os.walk(second_brain_path):
+    for file in files:
+      print(file)
+      if file.endswith(".md"):
+        file_path = os.path.join(root, file)
+        # use published frontmatter instead
+        with open(file_path, "r") as f:
+          fmt = frontmatter.load(f)
+          # print(fmt.metadata)
+        if "published" in fmt.metadata.keys() and fmt.metadata["published"] == True:
 
-def find_hashtag(second_brain_path: str, copy_to_path: str) -> None:
-    """find hashtag in my private SecondBrain and move it to public Brain.
-    - replace h1 title (`# ..`) with frontmatter as YAML title
-    - rename file names to lower-case
-    """
-    for root, dirs, files in os.walk(second_brain_path):
-        for file in files:
-            if file.endswith(".md"):
-                file_path = os.path.join(root, file)
-                with open(file_path, "r") as f:
-                    for line_number, line in reversed(list(enumerate(f, 1))):
-                        if "#publish" in line:
-                            # destination should be lower-case (spaces will be handled by hugo with `urlize`)
-                            file_name_lower = os.path.basename(file_path).lower()
+          # destination should be lower-case (spaces will be handled by hugo with `urlize`)
+          # file_name_lower = os.path.basename(file_path).lower() # HACK unnecessary?
 
-                            # print(f"publish: {file_path}, ln: {line_number}")
-                            # copy that file to the publish notes directory
-                            shutil.copy(
-                                file_path, os.path.join(copy_to_path, file_name_lower)
-                            )
-                            # get last modified date file_path
-                            last_modified = datetime.utcfromtimestamp(
-                                os.path.getmtime(file_path)
-                            ).strftime("%Y-%m-%d %H:%M:%S")
-
-                            # add h1 as title frontmatter
-                            add_h1_as_title_frontmatter(
-                                os.path.join(copy_to_path, file), last_modified
-                            )
-                            break
+          print(f"publish: {file_path}")
+          # copy that file to the publish notes directory
+          shutil.copy(
+            # file_path, os.path.join(copy_to_path, file_name_lower) # HACK unnecessary?
+            file_path, os.path.join(copy_to_path, file)
+          )
+          
+          # add h1 as title frontmatter
+          add_h1_as_title_frontmatter(
+            os.path.join(copy_to_path, file)
+          )
+          break
 
 
 def add_h1_as_title_frontmatter(file_path: str, last_modified: str):
